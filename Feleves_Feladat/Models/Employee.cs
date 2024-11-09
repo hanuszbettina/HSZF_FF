8b59011895656db7808e9ea152150c4717db4e6c
hanuszbettina <hanuszbettina.sulis@gmail.com>
2024-11-09 13:20:42 +0100

    Employee class

---
+++ Feleves_Feladat/Models/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Feleves_Feladat.Models
+{
+    public class Employee
+    {
+        public string? Name { get; set; }
+        public int BirthYear { get; set; }
+        public int StartYear { get; set; }
+        public int CompletedProjects { get; set; }
+        public bool Active { get; set; }
+        public bool Retired { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Job { get; set; }
+        public string? Level { get; set; }
+        public int Salary { get; set; }
+        public int Commission { get; set; }
+        public virtual ICollection<Department> Departments { get; set; } = new List<Department>();
+
+    }
+}
