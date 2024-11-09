4091fbf62b9414a058b3ac70f9ecf832ead64ff5
hanuszbettina <hanuszbettina.sulis@gmail.com>
2024-11-09 13:13:17 +0100

    Department class

---
+++ Feleves_Feladat/Models/Department.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feleves_Feladat.Models
+{
+    public class Department
+    {
+        public string? Name { get; set; }
+
+        public string? DepartmentCode { get; set; }
+
+        public string? HeadOfDepartment { get; set; }
+    }
+}
